c92d2aa0a40428568dd76e7f206cf11ffd405fe8
nlee-ncino <nathaniel.lee@ncino.com>
2025-07-22 16:53:07 -0400

    inc thread count

--- server/playwright.config.ts
+++ server/playwright.config.ts
@@ -4,8 +4,8 @@ export default defineConfig({
     testDir: "./tests",
     fullyParallel: true,
     forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 2 : 0,
-    workers: process.env.CI ? 1 : undefined,
+    retries: 2,
+    workers: 30,
     reporter: [["html", {open: "never"}]],
     use: {
         trace: 'off',
--- server/tests/testSetup.ts
+++ server/tests/testSetup.ts
@@ -6,7 +6,7 @@ export const test = base.extend({
         const commonContextOptions = {
             userAgent:
                 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-            viewport: {width: 1920, height: 1080},
+            viewport: {width: 1280, height: 720},
             javaScriptEnabled: true,
             bypassCSP: true,
             ignoreHTTPSErrors: true,
